1cffdd23ed51df55661a3b6f629bbdc5efee2040
Amir Arab <amir4rab@gmail.com>
2022-04-11 17:20:37 +0430

    util files added

---
+++ src/utils/backend/readAllFiles.ts
@@ -0,0 +1,29 @@
+import fs from 'fs';
+
+import { BlogHeaderArray, BlogHeader } from '@/types/blogHeader';
+import matter from 'gray-matter';
+
+const readMarkdown = async ( url: string ) => {
+  const path = process.cwd() + '/' + url;
+  const fileNames = fs.readdirSync(path);
+  const resultArray: BlogHeaderArray = [];
+  
+  fileNames.forEach( item => {
+    const markdownContent = fs.readFileSync(path + '/' + item, 'utf-8');
+    const { data } = matter(markdownContent);
+    resultArray.push({
+      ...data,
+      slug: ( data.title as string ).toLowerCase().replaceAll(' ', '-')
+    } as BlogHeader);
+  });
+
+  const sortedArray = resultArray.sort((a, b) => {
+    if ( a.date < b.date ) return 1;
+    if ( a.date > b.date ) return -1;
+    return 0;
+  })
+  
+  return sortedArray;
+}
+
+export default readMarkdown;
---
+++ src/utils/backend/readMarkdown.ts
@@ -0,0 +1,22 @@
+import fs from 'fs';
+
+import { unified } from 'unified';
+
+import remarkParse from 'remark-parse';
+import remarkHtml from 'remark-html';
+import matter from 'gray-matter';
+
+import { BlogHeader } from '@/types/blogHeader';
+
+const readMarkdown = async ( url: string ) => {
+  const path = process.cwd() + '/' + url;
+  const markdownContent = fs.readFileSync(path, 'utf-8');
+
+  const { data, content } = matter(markdownContent);
+
+  const htmlContent = unified().use(remarkParse).use(remarkHtml).processSync(content);
+  
+  return ({ header: data as BlogHeader, html: htmlContent.toString(), markdown: content });
+}
+
+export default readMarkdown;
