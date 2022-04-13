e5f5ff4fec12a294236a6ed97fbc1484e12a79d0
Amir Arab <amir4rab@gmail.com>
2022-04-13 09:25:34 +0430

    added twitter card info to blog pages

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { GetStaticProps, GetStaticPaths, NextPage } from 'next';
+import Head from 'next/head';
 
 // back end utils //
 import readAllFiles from '@/backend-utils/readAllFiles';
@@ -9,7 +10,23 @@ import readMarkdown from '@/backend-utils/readMarkdown';
 import BlogWrapper, { BlogProps } from '@/components/blogWrapper';
 
 const PaginatedHome:NextPage<BlogProps> = ( props ) => {
-  return (<BlogWrapper { ...props } />)
+  return (
+  <>
+    <Head>
+      <meta name='twitter:card' content={ props.blogHeader.shortInfo } />
+      <meta name='twitter:url' content='https://uni-project.amir4rab.com/' />
+      <meta name='twitter:title' content={ props.blogHeader.title } />
+      <meta name='twitter:description' content={ props.blogHeader.shortInfo } />
+      <meta name='twitter:image' content={ props.blogHeader.thumbnail } />
+      <meta property='og:type' content='website' />
+      <meta property='og:title' content={ props.blogHeader.title } />
+      <meta property='og:description' content={ props.blogHeader.shortInfo } />
+      <meta property='og:site_name' content='My Blog' />
+      <meta property='og:url' content='https://uni-project.amir4rab.com' />
+    </Head>
+    <BlogWrapper { ...props } />
+  </>
+  )
 }
 
 export const getStaticProps: GetStaticProps<BlogProps> = async (context) => {
