a647fffa9169b31cbafc77ffa591ea33791238cd
Amir Arab <amir4rab@gmail.com>
2022-05-30 21:52:41 +0430

    added packages

--- next.config.js
+++ next.config.js
@@ -4,6 +4,12 @@ const nextTranslate = require('next-translate')
 const nextConfig = nextTranslate({
   reactStrictMode: true,
   swcMinify: true,
+  compiler: {
+    styledComponents: true,
+  },
+  experimental: {
+    outputStandalone: process.env.DOCKER_BUILD == 1 ? true : false,
+  },
 })
 
 module.exports = nextConfig
